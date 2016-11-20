81e1a732653beacda3f7160ca1e7f0f64ce6dfbd
RayLiu-1 <ruli7792@colorado.edu>
2016-11-19 20:38:49 -0700

    debug

--- dfc.c
+++ dfc.c
@@ -91,7 +91,7 @@ int main(int argc, char * argv[]) {
 			//puts(username);
 			int n = recv(sock[i], readbuf[i], BUFSIZE, 0);
 			puts(readbuf[i]);
-			if (n = -1) {
+			if (n == -1) {
 				connect[i] = 0;
 				printf("server%d disconnected\n", i + 1);
 				close(sock[i]);
@@ -100,7 +100,7 @@ int main(int argc, char * argv[]) {
 
 			write(sock[i], password, strlen(password));
 			n = recv(sock[i], readbuf[i], BUFSIZE, 0);
-			if (n = -1) {
+			if (n == -1) {
 				connect[i] = 0;
 				printf("server%d disconnected\n",i+1);
 				close(sock[i]);

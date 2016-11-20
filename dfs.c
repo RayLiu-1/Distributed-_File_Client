694bf93718306155d6641fe0d14de62bc73c65a3
RayLiu-1 <ruli7792@colorado.edu>
2016-11-19 20:20:36 -0700

    test

--- dfs.c
+++ dfs.c
@@ -94,7 +94,7 @@ void *connection_handler(void *sockfd) {
 		for (i = 0; i < userindex; i++) {
 			validUser[i] = 0;
 		}
-		write(clfd, readbuf, strlen(sendbuf));
+		write(clfd, readbuf, strlen(readbuf));
 		for (i = 0; i < userindex; i++) {
 			if (strcmp(username[i], readbuf)==0) {
 				validUser[i] = 1;

eb47c3748aeae795dc6ec960ca5618deeb3509f6
RayLiu-1 <ruli7792@colorado.edu>
2016-11-19 22:56:57 -0700

    test

--- dfc.c
+++ dfc.c
@@ -123,6 +123,7 @@ int main(int argc, char * argv[]) {
 				for (int i = 0; i < 4; i++) {
 					if (connect[i] == 1) {
 						while (read_size = recv(sock[i], readbuf[i], BUFSIZE, 0)>0) {
+							if(strcmp(readbuf,".\n")!=0 && strcmp(readbuf, "..\n") != 0 && strcmp(readbuf, "d") != 0)
 							printf(readbuf[i]);
 						}
 					}

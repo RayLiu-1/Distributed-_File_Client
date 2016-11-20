1e5dab39a927f51b4f13f8d0cb087f3bcc1a6dac
RayLiu-1 <ruli7792@colorado.edu>
2016-11-20 01:50:49 -0700

    test

--- dfs.c
+++ dfs.c
@@ -187,9 +187,8 @@ void *connection_handler(void *sockfd) {
 			dp = opendir(filepath);
 			if (dp) {
 				while ((dir = readdir(dp)) != NULL ) {
-					sprintf(sendbuf, "%s", dir->d_name);
-					write(clfd, sendbuf, strlen(sendbuf)+1);
-					printf("%d", strlen(sendbuf) + 1);
+					write(clfd, dir->d_name, strlen(dir->d_name)+1);
+					printf("%d", strlen(dir->d_name) + 1);
 				}
 				
 				closedir(dp);

2b8a74122f8bf28e8cc92c9bd48d558486f2ed73
RayLiu-1 <ruli7792@colorado.edu>
2016-11-19 22:21:54 -0700

    test

--- dfc.c
+++ dfc.c
@@ -114,6 +114,7 @@ int main(int argc, char * argv[]) {
 			printf("Please enter command: ");
 			char commond[BUFSIZE];
 			fgets(commond, BUFSIZE, stdin);
+			puts(commond);
 			if ((strncmp(commond, "LIST ",5) == 0)||(strcmp(commond,"LIST")==0)) {
 				for (int i = 0; i < 4; i++)
 				{	

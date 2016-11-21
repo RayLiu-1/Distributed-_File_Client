8ba56ccd9a4122f430a06957ec3249d6e02d6185
RayLiu-1 <ruli7792@colorado.edu>
2016-11-20 17:41:23 -0700

    test

--- dfc.c
+++ dfc.c
@@ -250,7 +250,9 @@ int hash_md5(char *file) {
 	char md5[200];
 	fscanf(fp, "%s", &md5);
 	printf("%s", md5);
-	return 0;
+	char lbit[2];
+	strncpy(lbit, md5 + md5(strlen(md5)) - 1, 1);
+	return strtol(lbit, NULL, 16) % 4;
 }
 
 //void *connection_handler(void *sockfd) {}
